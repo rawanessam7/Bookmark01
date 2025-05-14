2e7aaf403b433adf4cc68869d9f6f559dba0ed04
rawanessam7 <rawanessam107@gmail.com>
2025-05-14 14:23:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let login=document.querySelector(".login");
+let signup=document.querySelector(".signup");
+function Switch(){
+    login.classList.toggle("hidden");
+    signup.classList.toggle("hidden");
+
+}
